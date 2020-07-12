1fb9f669c4b810e79b6e3dbcdb23161d0f45febe
Alex Parinov <creafz@gmail.com>
2020-07-12 17:10:48 +0300

    Add examples

--- mkdocs/src/hooks.py
+++ mkdocs/src/hooks.py
@@ -1,16 +1,42 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+import json
+import os
 
 from bs4 import BeautifulSoup
+from mknotebooks.plugin import NotebookFile
 
 
-SITE_NETLOC = "albumentations.ai"
+def on_pre_page(page, config, files):
+    if isinstance(page.file, NotebookFile):
+        filename = os.path.basename(page.file.src_path)
+        page.is_jupyter_notebook = True
+        page.notebook_github_url = urljoin(config["notebook_github_uri"], filename)
+        page.notebook_colab_url = urljoin(config["notebook_colab_uri"], filename)
+    return page
 
 
-def external_links_target_blank(html, page, config, **kwargs):
+def on_page_content(html, page, config, **kwargs):
+    site_netloc = urlparse(config["site_url"]).netloc
     soup = BeautifulSoup(html, "html.parser")
     links = soup.find_all("a", href=True)
     for link in links:
         netloc = urlparse(link["href"]).netloc
-        if netloc and netloc != SITE_NETLOC:
+        if netloc and netloc != site_netloc:
             link["target"] = "_blank"
+
     return str(soup)
+
+
+def on_nav(nav, config, files):
+    for page in nav.pages:
+        if isinstance(page.file, NotebookFile):
+            with open(page.file.abs_src_path) as f:
+                content = json.load(f)
+                for cell in content["cells"]:
+                    if cell["cell_type"] != "markdown":
+                        continue
+                    source = cell["source"]
+                    if source and source[0].startswith("# "):
+                        page.title = source[0][2:]
+                        break
+    return nav
