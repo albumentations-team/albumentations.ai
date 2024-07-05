eabe051fa4ea84248241b3cead22fa9e3f1748b8
Vladimir Iglovikov <iglovikov@gmail.com>
2024-07-04 18:29:26 -0700

    More logs

--- builder/src/lib/pypi.py
+++ builder/src/lib/pypi.py
@@ -19,23 +19,29 @@ def get_pypi_download_count(package_name: str, timeout: int = 120) -> int:
     if not credentials_path.exists():
         raise ValueError("Google credentials file not found.")
 
-    package_name = "albumentations"
-
     set_google_credentials_path(credentials_path.as_posix())
 
     command = ["pypinfo", "--json", package_name]
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True, timeout=timeout)
+        logger.info(f"Command Output: {result.stdout}")
+        logger.info(f"Command Error Output: {result.stderr}")
         output = json.loads(result.stdout)
         return output["rows"][0]["download_count"]
+    except subprocess.TimeoutExpired as e:
+        logger.exception(f"Command '{e.cmd}' timed out after {timeout} seconds.")
+        raise TimeoutError(f"Command '{e.cmd}' timed out after {timeout} seconds.") from e
     except subprocess.CalledProcessError as e:
         logger.exception(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}.")
-        logger.exception(f"Output: {e.output}")
-        raise ValueError(f"Failed to get download count. Error: {e}.") from e
+        logger.exception(f"Command Output: {e.output}")
+        logger.exception(f"Command Error Output: {e.stderr}")
+        raise ValueError(f"Failed to get download count for {package_name}.") from e
     except json.JSONDecodeError as e:
         logger.exception("Failed to parse JSON output.")
         logger.exception(f"Error: {e}")
-        raise ValueError(f"Failed to parse JSON output Error: {e}.") from e
+        logger.exception(f"Command Output: {result.stdout}")
+        logger.exception(f"Command Error Output: {result.stderr}")
+        raise ValueError(f"Failed to parse JSON output. Error: {e}.") from e
     except Exception as e:
         logger.exception(f"An error occurred: {e}")
         raise ValueError(f"Failed to get download count. Error: {e}.") from e
