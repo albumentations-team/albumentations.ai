7d5eda424c3b087477ab94bfcc4712968e5678c4
Vladimir Iglovikov <iglovikov@gmail.com>
2024-11-15 16:48:53 -0800

    Added root prefix

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build',
   basePath,
+  assetPrefix: '/',
   trailingSlash: true,
   images: {
     unoptimized: true,
